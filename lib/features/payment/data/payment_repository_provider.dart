7293f8ee5913fba782ec82cd77f8f9cb66ba82c4
Hussain SK <46426910+hussainxgit@users.noreply.github.com>
2025-03-28 14:42:41 +0300

    Delete lib/features/payment directory

--- lib/features/payment/data/payment_repository_provider.dart
+++
@@ -1,9 +0,0 @@
-// lib/features/payment/data/payment_repository_provider.dart
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../../core/firebase/firebase_providers.dart';
-import 'repository/payment_repository.dart';
-
-final paymentRepositoryProvider = Provider<PaymentRepository>((ref) {
-  final firestore = ref.watch(firestoreProvider);
-  return PaymentRepositoryImpl(firestore: firestore);
-});
